63b62b87f6845792318850d85a12997262e9c393
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-01 13:58:26 +0500

    Add `WritableBufferInterface` to define contract for writable buffer operations

---
+++ src/Buffers/WritableBufferInterface.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Part of the "charcoal-dev/contracts" package.
+ * @link https://github.com/charcoal-dev/contracts
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Contracts\Buffers;
+
+/**
+ * WritableBufferInterface extends ByteArrayInterface to facilitate operations
+ * involving byte manipulation and storage, specifically allowing for buffer
+ * modifications such as writing data.
+ */
+interface WritableBufferInterface extends ByteArrayInterface
+{
+    public function toImmutable(): ImmutableBufferInterface;
+}
