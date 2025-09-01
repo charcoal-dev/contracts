c8584bc26d38ad07595b99e1d4e2f523abd0e4f2
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-01 05:52:34 +0500

    Add core contract interfaces and enums for server and HTTP APIs

---
+++ src/Http/HttpServerRequestInterface.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Part of the "charcoal-dev/contracts" package.
+ * @link https://github.com/charcoal-dev/contracts
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Contracts\Http;
+
+/**
+ * Defines a type marker for server request objects.
+ */
+interface HttpServerRequestInterface extends HttpRequestInterface
+{
+}
---
+++ src/ServerApiInterface.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Part of the "charcoal-dev/contracts" package.
+ * @link https://github.com/charcoal-dev/contracts
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Contracts;
+
+use Charcoal\Contracts\Enums\SapiType;
+
+/**
+ * Represents the interface for the server API, providing a type marker,
+ * that define the contract to be implemented by classes interacting with the server.
+ * @property-read SapiType $type The type of server API (SAPI) being used.
+ */
+interface ServerApiInterface
+{
+}
