79ce8655911e291b2a22cf0cb516484418d78681
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-01 18:19:39 +0500

    Add `BufferEncoderInterface` and extend `EncodingSchemeInterface` with encoding and decoding methods

--- src/Encoding/EncodingSchemeInterface.php
+++ src/Encoding/EncodingSchemeInterface.php
@@ -8,9 +8,18 @@ declare(strict_types=1);
 
 namespace Charcoal\Contracts\Encoding;
 
+use Charcoal\Contracts\Buffers\ByteArrayInterface;
+use Charcoal\Contracts\Buffers\ReadableBufferInterface;
+
 /**
  * Represents a marker for encoding-related functionality.
+ * Can be used with an Enum instead of concrete classes.
  */
 interface EncodingSchemeInterface
 {
+    public function encoder(): ?BufferEncoderInterface;
+
+    public function encode(ReadableBufferInterface $data): string;
+
+    public function decode(string $data): ByteArrayInterface;
 }
