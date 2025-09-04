06c7bdde5a685d778c46fdcccc904879de13502d
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-04 19:56:31 +0500

    Add `StorageProviderInterface` and `StorageTypeEnumInterface`

---
+++ src/Storage/StorageTypeEnumInterface.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Part of the "charcoal-dev/contracts" package.
+ * @link https://github.com/charcoal-dev/contracts
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Contracts\Storage;
+
+/**
+ * Represents a contract for enumerating storage types.
+ * It extends the BackedEnum interface, which provides functionality
+ * for managing and interacting with enumerations in PHP.
+ */
+interface StorageTypeEnumInterface extends \BackedEnum
+{
+}
