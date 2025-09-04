4d67f16d699ebf5750368dc26f228d83aab0c3c5
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-04 20:32:19 +0500

    Add `StringVectorInterface` for handling string-based vectors

---
+++ src/Vectors/StringVectorInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Part of the "charcoal-dev/contracts" package.
+ * @link https://github.com/charcoal-dev/contracts
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Contracts\Vectors;
+
+/**
+ * Represents an interface for a vector that specifically handles strings.
+ * Extends the functionality of a general vector interface.
+ * @implements VectorInterface<string>
+ */
+interface StringVectorInterface extends VectorInterface
+{
+}
