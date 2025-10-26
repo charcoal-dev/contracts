9f8441c057315937f6497a2f8b6eb6fb90545d42
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-10-27 01:51:15 +0500

    Add `ValidationExceptionInterface` for validation exception contracts

---
+++ src/Sapi/ValidationExceptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the "charcoal-dev/contracts" package.
+ * @link https://github.com/charcoal-dev/contracts
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Contracts\Sapi;
+
+/**
+ * Interface ValidationExceptionInterface
+ * @package Charcoal\Contracts\Sapi
+ */
+interface ValidationExceptionInterface extends \Throwable
+{
+}
